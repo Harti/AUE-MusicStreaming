6d8dc112cd7ecea0ee65edde54da59cff57c1288
Hartmut Glücker <hartgluecker@gmx.net>
2014-07-29 16:40:30 +0200

    added success view

---
+++ app/views/screener/success.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.framework')
+
+@section('page-title'){{ "Vielen Dank für deine Teilnahme!" }}@stop
+
+@section('page-navigation')
+<div id="screenerHeadUnit">
+	<div class="row">
+		<div class="small-12 medium-4 columns" id="logo"></div>
+		<div class="small-12 medium-8 columns text-right">
+			<h1>Übertragung erfolgreich</h1>
+		</div>
+	</div>
+</div>
+@stop
+
+@section('page-content')
+<h2>Vielen Dank für deine Teilnahme!</h2>
+<p>Wie versprochen gibt es jetzt noch die Möglichkeit für dich, einen Amazon-Gutschein abzustauben:</p>
+<p>Wir benötigen nämlich Freiwillige, die an einer so genannten "Tagebuch-Studie" teilnehmen. Die Studie sähe so aus, dass wir dir mitteilen, welchen der vier Streaming-Dienste du <strong>über einen Zeitraum von fünf Tagen</strong> benutzen sollst &ndash; und du müsstest jeden Abend eine kurze Umfrage (wieder ca. 10-15 Minuten) ausfüllen.</p>
+<hr />
+@stop
