0f618e04f70daf3e718568d60e0f968049676f3d
Hartmut Glücker <hartgluecker@gmx.net>
2014-07-29 16:39:26 +0200

    changed screener attempt schema

--- app/database/migrations/2014_07_28_173424_screener_attempts.php
+++ app/database/migrations/2014_07_28_173424_screener_attempts.php
@@ -18,6 +18,16 @@ class ScreenerAttempts extends Migration {
 			$table->timestamp('created_at');
 			$table->timestamp('updated_at');
 			$table->timestamp('completed_at')->nullable();
+			$table->string('preferred_service')->nullable();
+			$table->string('preferred_known')->nullable();
+			$table->string('preferred_look')->nullable();
+			$table->string('preferred_features')->nullable();
+			$table->string('preferred_other')->nullable();
+			$table->string('disliked_service')->nullable();
+			$table->string('disliked_known')->nullable();
+			$table->string('disliked_look')->nullable();
+			$table->string('disliked_features')->nullable();
+			$table->string('disliked_other')->nullable();
 		});
 	}
 
