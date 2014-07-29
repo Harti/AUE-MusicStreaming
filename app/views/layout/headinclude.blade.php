04906e1173c3c37467baf661ec95caa54bbf628f
Hartmut Glücker <hartgluecker@gmx.net>
2014-07-29 16:40:09 +0200

    included jQueryUI

--- app/views/layout/headinclude.blade.php
+++ app/views/layout/headinclude.blade.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}" />
 <link rel="stylesheet" href="{{ URL::asset('css/foundation.min.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/jquery-ui.min.css') }}" />
 <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}" />
 <link rel="stylesheet" href="{{ URL::asset('css/global.css') }}" />
 @yield('page-css')
--- app/views/layout/scriptinclude.blade.php
+++ app/views/layout/scriptinclude.blade.php
@@ -1,4 +1,5 @@
 <script type="text/javascript" src="{{ URL::asset('js/vendor/jquery.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/jquery-ui.min.js') }}"></script>
 <script type="text/javascript" src="{{ URL::asset('js/vendor/modernizr.js') }}"></script>
 <script type="text/javascript" src="{{ URL::asset('js/foundation.min.js') }}"></script>
 <script>
