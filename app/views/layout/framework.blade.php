358983bbf4be6bc7ec4910e96d7724f482b6a83f
Hartmut Gluecker <hartgluecker@gmx.net>
2014-09-06 19:44:27 +0200

    added imprint

--- app/views/layout/framework.blade.php
+++ app/views/layout/framework.blade.php
@@ -13,6 +13,21 @@
 @yield('page-content')
 </div>
 
+
+<a id="imprint" href="javascript:void(0);" data-reveal-id="imprintModal">Impressum</a>
+<div id="imprintModal" class="reveal-modal" data-reveal>
+	<h2>Impressum</h2>
+	<p class="lead">Dieses Projekt wird im Rahmen des Kurses "Advanced Usability Engineering" am Lehrstuhl für Medieninformatik der Universität Regensburg durchgeführt.</p>
+	<p>Hauptverantwortlicher für das Projekt:</p>
+	<p>
+		H. Glücker<br />
+		Brandenburger Str. 7<br />
+		93057 Regensburg<br />
+		<br />
+		hartgluecker@gmx.net
+	</p>
+	<a class="close-reveal-modal">&#215;</a>
+</div>
 @include('layout.scriptinclude')	
 </body>
 </html>
