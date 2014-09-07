559f6c0d66eccabf64d7b2e7ff48a78e7415a4f3
Hartmut Gluecker <hartgluecker@gmx.net>
2014-09-07 22:40:53 +0200

    removed UTF8 bug

--- public/js/foundation-datepicker.js
+++ public/js/foundation-datepicker.js
@@ -830,8 +830,8 @@
 		    days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
 		    daysShort: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
 		    daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
-		    months: ["Januar", "Februar", "MÃ¤rz", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
-		    monthsShort: ["Jan", "Feb", "MÃ¤r", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+		    months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+		    monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
 		    today: "Heute"
 		}
 	};
