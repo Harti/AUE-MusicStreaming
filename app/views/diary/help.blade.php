7fc356c9dc6f415fb9ff899beead748fa98f4b5e
Hartmut Glücker <hartgluecker@gmx.net>
2014-09-08 15:49:39 +0200

    added headline to help section

--- app/views/diary/help.blade.php
+++ app/views/diary/help.blade.php
@@ -2,6 +2,9 @@
 
 @section('page-content')
 <div class="row">	
+    <div class="small-12 columns">
+        <h2><i class="fa fa-question-circle fa-lg"></i> Hilfe</h2>
+    </div>
 @include('help')
 </div>
 @stop
