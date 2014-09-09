2834e3eb71cff69eaab405139039aeaeb8c90edd
blackcookiejar <black.cookie.jar@gmail.com>
2014-09-09 10:54:28 +0200

    fixed bug that occurred when sliders fell below zero

--- public/js/diaryEntry.js
+++ public/js/diaryEntry.js
@@ -87,7 +87,6 @@ $(function(){
     var jquerySliderContainers = $(".slider-group .slider-container");
     function updateOtherSliders(sliderContainer, value){
         var total = 0,
-            numActiveSliders = 0,
             sliderInput = $(sliderContainer).find(".slider-input");
             otherSliders = $(jquerySliderContainers).not(sliderContainer);
 
@@ -99,7 +98,6 @@ $(function(){
                 return;
             } else {
                 total += otherValue;
-                numActiveSliders++;
             }
         });
 
@@ -107,27 +105,64 @@ $(function(){
 
         var delta = 100 - total;
 
+        function math_shit(values, min, delta, ignore_indices){
+            var new_delta = 0;
+            var new_ignore_indices = [];
+
+            // Check for sliders that are 0 and ignore them
+            for(var i = 0; i < values.length; i++){
+                if(values[i] === min && ignore_indices.indexOf(i) === -1){
+                    ignore_indices.push(i);
+                }
+            }
+
+            var numActiveIndices = values.length - ignore_indices.length;
+
+            // Calculate the new values for all active sliders
+            // If the value of a slider falls below 0, add it to the ignore list
+            // and add its negative value to the new delta
+            for(var idx = 0; idx < values.length; idx++){
+
+                // skip the ignored sliders
+                if(ignore_indices.indexOf(idx) > -1){
+                    continue;
+                }
+
+                values[idx] += delta/numActiveIndices;
+
+                // check if value would be below zero
+                if (values[idx] < min){
+                    new_delta += values[idx];
+                    values[idx] = min;
+                }
+            }
+
+            // If there is a new delta, do the same thing again, with 
+            // the rest of the active sliders, excluding the newly added
+            // ignored sliders
+            if(new_delta !== 0){
+                math_shit(values, min, new_delta, ignore_indices);
+            }
+        }
+
+        var sliderValues = [];
         otherSliders.each(function(){
-            var currentValue = parseInt($(this).find(".jquery-slider").slider("value"));
-            var newValue = 0;
+            sliderValues.push(parseInt($(this).find(".jquery-slider").slider("value")));
+        });
 
-            if(numActiveSliders === 0){
-                newValue = currentValue + (delta/otherSliders.length);
-            } else {
-                newValue = currentValue + (delta/numActiveSliders);
-            }
+        math_shit(sliderValues, 0, delta, []);
 
-            if (newValue < 0){
-                newValue = 0;
-            } else if (newValue > 100){
-                newValue = 100;
-            } else if (currentValue === 0){
-                newValue = 0;
-            }
-
+        otherSliders.each(function(index){
+            var newValue = sliderValues[index];
             $(this).find(".jquery-slider").slider('value', newValue);
             $(this).find(".slider-input").val(Math.round(newValue*10)/10);
         });
+
+        var sum = 0;
+        jquerySliderContainers.each(function(){
+            sum += parseFloat($(this).find(".slider-input").val());
+        });
+        console.log("Values sum: " + sum);
     }
 
     jquerySliderContainers.each(function(){
